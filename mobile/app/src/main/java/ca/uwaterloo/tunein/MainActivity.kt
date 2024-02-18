8309a38a9c82f280af47844e861c0e2e032dc2b3
Rishi Shah <60271537+rishi-shah12@users.noreply.github.com>
2024-02-18 12:23:29 -0500

    Landing Page

--- mobile/app/src/main/java/ca/uwaterloo/tunein/MainActivity.kt
+++ mobile/app/src/main/java/ca/uwaterloo/tunein/MainActivity.kt
@@ -3,14 +3,30 @@ package ca.uwaterloo.tunein
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+//import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ca.uwaterloo.tunein.ui.theme.TuneInTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.*
+import java.io.File
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,27 +36,41 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xFF003847)
                 ) {
-                    Greeting("World")
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Spacer(modifier = Modifier
+                            .height(64.dp)
+                            .fillMaxWidth())
+                        Image(
+                            painter = painterResource(id = R.drawable.logo_fill),
+                            contentDescription = "Content"
+                        )
+//                        Box(
+//                            modifier = Modifier
+//                                .size(250.dp)
+//                                .background(color = Color.Blue, shape = RoundedCornerShape(8.dp))
+//                        )
+                        Spacer(modifier = Modifier.height(192.dp))
+                        Button(
+                            onClick = {/*TODO*/ },
+                            colors= ButtonDefaults.buttonColors(containerColor = Color(0xFF00FC64), contentColor = Color.Black),
+                            modifier = Modifier.width(192.dp)
+                        ) {
+                            Text(text = "Log In")
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Button(
+                            onClick = {/*TODO*/ },
+                            colors= ButtonDefaults.buttonColors(containerColor = Color(0xFF00FC64), contentColor = Color.Black),
+                            modifier = Modifier.width(192.dp)
+                        ) {
+                            Text(text = "Sign Up")
+                        }
+                    }
                 }
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TuneInTheme {
-        Greeting("World")
-    }
-}
+//
