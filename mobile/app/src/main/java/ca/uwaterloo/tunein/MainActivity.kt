e46613af7632919bdc604292456975acc0773443
Adrian Jendo <ajendo@uwaterloo.ca>
2024-02-19 14:40:32 -0500

    Update activities with previews

--- mobile/app/src/main/java/ca/uwaterloo/tunein/MainActivity.kt
+++ mobile/app/src/main/java/ca/uwaterloo/tunein/MainActivity.kt
@@ -4,78 +4,79 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-//import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ca.uwaterloo.tunein.ui.theme.TuneInTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.*
-import java.io.File
 import android.content.Intent
-
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fun handleSignUp() {
+            val intent = Intent(this@MainActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
         setContent {
-            TuneInTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color(0xFF003847)
+            MainScreen { handleSignUp() }
+
+        }
+    }
+}
+
+@Composable
+fun MainScreen(handleSignUp: () -> Unit) {
+    TuneInTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color(0xFF003847)
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Spacer(modifier = Modifier
+                    .height(64.dp)
+                    .fillMaxWidth())
+                Image(
+                    painter = painterResource(id = R.drawable.logo_fill),
+                    contentDescription = "Content"
+                )
+                Spacer(modifier = Modifier.height(192.dp))
+                Button(
+                    onClick = {/*TODO*/ },
+                    colors= ButtonDefaults.buttonColors(containerColor = Color(0xFF00FC64), contentColor = Color.Black),
+                    modifier = Modifier.width(192.dp)
                 ) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Spacer(modifier = Modifier
-                            .height(64.dp)
-                            .fillMaxWidth())
-                        Image(
-                            painter = painterResource(id = R.drawable.logo_fill),
-                            contentDescription = "Content"
-                        )
-//                        Box(
-//                            modifier = Modifier
-//                                .size(250.dp)
-//                                .background(color = Color.Blue, shape = RoundedCornerShape(8.dp))
-//                        )
-                        Spacer(modifier = Modifier.height(192.dp))
-                        Button(
-                            onClick = {/*TODO*/ },
-                            colors= ButtonDefaults.buttonColors(containerColor = Color(0xFF00FC64), contentColor = Color.Black),
-                            modifier = Modifier.width(192.dp)
-                        ) {
-                            Text(text = "Log In")
-                        }
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Button(
-                            onClick = {
-                                val intent = Intent(this@MainActivity, SignUpActivity::class.java)
-                                startActivity(intent)
-                            },
-                            colors= ButtonDefaults.buttonColors(containerColor = Color(0xFF00FC64), contentColor = Color.Black),
-                            modifier = Modifier.width(192.dp)
-                        ) {
-                            Text(text = "Sign Up")
-                        }
-                    }
+                    Text(text = "Log In")
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = {
+                        handleSignUp()
+                    },
+                    colors= ButtonDefaults.buttonColors(containerColor = Color(0xFF00FC64), contentColor = Color.Black),
+                    modifier = Modifier.width(192.dp)
+                ) {
+                    Text(text = "Sign Up")
                 }
             }
         }
     }
 }
-//
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    MainScreen({})
+}
--- mobile/app/src/main/java/ca/uwaterloo/tunein/SignUpActivity.kt
+++ mobile/app/src/main/java/ca/uwaterloo/tunein/SignUpActivity.kt
@@ -19,87 +19,94 @@ import androidx.compose.ui.text.font.FontWeight
 import ca.uwaterloo.tunein.ui.theme.TuneInTheme
 import androidx.compose.foundation.border
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.tooling.preview.Preview
 
 class SignUpActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TuneInTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color(0xFF003847)
-                ) {
-                    SignUpScreen()
-                }
-            }
+            SignUpScreen()
         }
     }
 }
 
 @Composable
 fun SignUpScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Spacer(modifier = Modifier.height(64.dp)) 
-        Image(
-            painter = painterResource(id = R.drawable.tunein_logo), 
-            contentDescription = "App Logo",
-            modifier = Modifier.size(69.dp) 
-        )
-        Spacer(modifier = Modifier.height(32.dp))
-        Text(
-            text = "Welcome!",
-            style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold, color = Color.White),
-            color = Color.White
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text(text = "First Name", color = Color.White, fontWeight = FontWeight.Light) },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text(text = "Last Name", color = Color.White, fontWeight = FontWeight.Light) },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text(text = "Email", color = Color.White, fontWeight = FontWeight.Light) },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text(text = "Password", color = Color.White, fontWeight = FontWeight.Light) },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text(text = "Confirm Password", color = Color.White, fontWeight = FontWeight.Light) },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        
-        Button(
-            onClick = { /* TODO */ },
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2BC990))
+    TuneInTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color(0xFF003847)
         ) {
-            Text("Sign Up")
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(64.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.tunein_logo),
+                    contentDescription = "App Logo",
+                    modifier = Modifier.size(69.dp)
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(
+                    text = "Welcome!",
+                    style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold, color = Color.White),
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = { Text(text = "First Name", color = Color.White, fontWeight = FontWeight.Light) },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = { Text(text = "Last Name", color = Color.White, fontWeight = FontWeight.Light) },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = { Text(text = "Email", color = Color.White, fontWeight = FontWeight.Light) },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = { Text(text = "Password", color = Color.White, fontWeight = FontWeight.Light) },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = { Text(text = "Confirm Password", color = Color.White, fontWeight = FontWeight.Light) },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = { /* TODO */ },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2BC990))
+                ) {
+                    Text("Sign Up")
+                }
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun SignUpScreenPreview() {
+    SignUpScreen()
 }
