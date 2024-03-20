8aff8109d9d945ced506df642fa6bf8977113a6e
Rishi Shah <60271537+rishi-shah12@users.noreply.github.com>
2024-03-20 13:48:20 -0400

    Committing changes

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -6,7 +6,11 @@ import { z } from "zod";
 import { eq, or } from "drizzle-orm";
 import { Plugin, paginationSchema, searchSchema } from "../types";
 import { generateLikeFilters } from "../lib/generateLikeFilters";
+<<<<<<< Updated upstream
 import { encrypt } from "../lib/encryption";
+=======
+import { CONNREFUSED } from "dns";
+>>>>>>> Stashed changes
 
 export const users: Plugin = (server, _, done) => {
   server.post(
@@ -224,6 +228,109 @@ export const users: Plugin = (server, _, done) => {
           .where(where);
 
         return res.code(200).send(users);
+      } catch (e) {
+        console.error(e);
+        return res.code(500).send({ error: "Internal server error." });
+      }
+    },
+  );
+
+  server.get(
+    "/most_recent_song/:identifier",
+    {
+      schema: {
+        params: z.object({
+          identifier: z.string(),
+        }),
+      },
+    },
+    async (req, res) => {
+      try {
+        const { identifier } = req.params;
+
+        const user = await db.query.userTable.findFirst({
+          where: or(eq(userTable.username, identifier), eq(userTable.id, identifier)),
+        });
+
+        if (!user) {
+          return res.code(404).send({ error: "User not found with given parameters." });
+        }
+        var access_token = user["spotifyAccessToken"] || "None"
+        var reset_token = user["spotifyRefreshToken"] || "None"
+
+        var starting = "Bearer "
+
+        const response = await fetch('https://api.spotify.com/v1/me/player/recently-played', {headers: {'Authorization': starting.concat(access_token)}})
+        // console.log(response.text())
+        const value = await response.json();
+        
+        if (Object.keys(value)[0] === "error") {
+          console.log("Need to refresh token")
+          
+          const url = "https://accounts.spotify.com/api/token";
+
+          const payload = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              Authorization: "Basic " + Buffer.from("68edc012e9bf47b7a7769a372b705080" + ":" + "e5ea9469569c443e83a2913bd3ad2bf7").toString("base64"),
+            },
+            body: new URLSearchParams({
+              grant_type: 'refresh_token',
+              refresh_token: reset_token,
+              client_id: "68edc012e9bf47b7a7769a372b705080"
+            }),
+          }
+          const refresh_response = await fetch(url, payload);
+          const refresh_response_json = await refresh_response.json();
+
+          console.log(refresh_response_json["access_token"])
+
+          await db.update(userTable)
+            .set({ spotifyAccessToken: refresh_response_json["access_token"] })
+            .where(eq(userTable.id, identifier));
+
+
+            const user = await db.query.userTable.findFirst({
+              where: or(eq(userTable.username, identifier), eq(userTable.id, identifier)),
+            });
+    
+            if (!user) {
+              return res.code(404).send({ error: "User not found with given parameters." });
+            }
+            var access_token = user["spotifyAccessToken"] || "None"
+            var reset_token = user["spotifyRefreshToken"] || "None"
+    
+            var starting = "Bearer "
+    
+            const response = await fetch('https://api.spotify.com/v1/me/player/recently-played', {headers: {'Authorization': starting.concat(access_token)}})
+            // console.log(response.text())
+            const value = await response.json();
+
+            var artist = value["items"][0]["track"]["album"]["artists"][0]["name"]
+            var album_art = value["items"][0]["track"]["album"]["images"][1]["url"]
+            var album_name = value["items"][0]["track"]["album"]["name"]
+            var track_name = value["items"][0]["track"]["name"]
+
+            var resp_obj = {"artist": artist, "album_art": album_art, "album_name": album_name, "track_name": track_name}
+            console.log(resp_obj)
+            return res.code(200).send(resp_obj);
+
+        }
+
+
+        else {
+          console.log("TOken is goood")
+          // console.log(value["items"][0])
+          var artist = value["items"][0]["track"]["album"]["artists"][0]["name"]
+          var album_art = value["items"][0]["track"]["album"]["images"][1]["url"]
+          var album_name = value["items"][0]["track"]["album"]["name"]
+          var track_name = value["items"][0]["track"]["name"]
+
+          var resp_obj = {"artist": artist, "album_art": album_art, "album_name": album_name, "track_name": track_name}
+          console.log(resp_obj)
+          return res.code(200).send(resp_obj);
+        }
       } catch (e) {
         console.error(e);
         return res.code(500).send({ error: "Internal server error." });
