22449d93192debecce5c945040bc9c5c89e2c0bf
Cameron Kinsella <53635597+cameronkinsella@users.noreply.github.com>
2024-03-29 16:24:47 -0400

    Add cronjob for sending notifications

--- api/src/lib/sendNotification.ts
+++ api/src/lib/sendNotification.ts
@@ -1,7 +1,10 @@
 import { initializeApp } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
-import { User } from "../db/schema";
 import admin from "firebase-admin";
+import { db } from "../db/db";
+import { userTable } from "../db/schema";
+import { ne } from "drizzle-orm";
+import { Params } from "fastify-cron";
 
 const serviceAccount = {
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
@@ -14,16 +17,37 @@ const app = initializeApp({
 });
 const messaging = getMessaging(app);
 
-export async function sendToDevice(user: User, title: string, body: string): Promise<string | null> {
+export async function sendToDevice(token: string, title: string, body: string): Promise<string | null> {
   const message = {
     notification: {
       title: title,
       body: body,
     },
-    token: user.androidRegistrationToken,
+    token: token,
   };
 
   // Send a message to the device corresponding to the provided
   // registration token.
   return await messaging.send(message);
 }
+
+export const notificationJob: Params = {
+  cronTime: "0 0 * * *", // Every day at midnight UTC
+
+  onTick: async (server) => {
+    // TODO: create new posts before sending notification
+    const result = await db
+      .select({
+        token: userTable.androidRegistrationToken,
+      })
+      .from(userTable)
+      .where(ne(userTable.androidRegistrationToken, ""));
+    for (const { token } of result) {
+      await sendToDevice(
+        token,
+        "Your daily TuneIn posts are here!",
+        "Open the app to see what your friends have been listening to",
+      );
+    }
+  },
+};
